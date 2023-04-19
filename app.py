9caf133e260e1743f365e646621d504faa70cfc3
Sathyaseelan <sathyamsd97@gmail.com>
2023-04-19 20:32:42 +0530

    Second commit

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask_smorest import Api
 from db import db
 import models
 
+
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
